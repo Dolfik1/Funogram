1fe6a84776687af61b78b4d5b8b48d9ad63ace0a
marctdt <marc.tdt@oultook.com>
2023-09-29 09:15:09 +0800

    Refactored processUpdate function to process multiple updates at once.

--- src/Funogram.Telegram/Bot.fs
+++ src/Funogram.Telegram/Bot.fs
@@ -46,17 +46,17 @@ type UpdateContext =
 // Text of the command; 1-32 characters. Can contain only lowercase English letters, digits and underscores.
 let inline private isAllowedChar (c: char) = Char.IsLetter c || Char.IsDigit c || c = '_'
 
-// returns -1 if the command is not valid otherwise index of last character 
+// returns -1 if the command is not valid otherwise index of last character
 let private validateCommand (text: string) =
   let rec iter (text: string) i len =
     if i >= len || isAllowedChar text.[i] |> not then
       (i - 1)
     else
       iter text (i + 1) len
-  
+
   if text.Length <= 1 || text.[0] <> '/' then -1
   else iter text 1 text.Length
-  
+
 let getTextForCommand (me: User) (textOriginal: string option) =
   match me.Username, textOriginal with
   | Some username, Some text when text.Length > 0 && text.[0] = '/' ->
@@ -67,12 +67,12 @@ let getTextForCommand (me: User) (textOriginal: string option) =
       text.Remove(idx + 1, username.Length + 1) |> Some
     | _ -> textOriginal
   | _ -> textOriginal
-  
+
 let checkCommand (context: UpdateContext) (command: string) =
   match context.Update.Message with
   | Some { Text = text } when (getTextForCommand context.Me text) = Some command -> true
   | _ -> false
-    
+
 let cmd (command: string) (handler: UpdateContext -> unit) (context: UpdateContext) =
   context.Update.Message
   |> Option.bind (fun message -> getTextForCommand context.Me message.Text)
@@ -116,12 +116,12 @@ let private runBot config me updateArrived updatesArrived =
             return! loopAsync offset // send new offset
           | Error e ->
             config.OnError (e.AsException() :> Exception)
-            
+
             // add delay in case of HTTP error
             // for example: the server may be "busy"
             if e.Description = "HTTP_ERROR" then
               do! Async.Sleep 1000
-            
+
             return! loopAsync offset
           | _ ->
             return! loopAsync offset
@@ -131,7 +131,7 @@ let private runBot config me updateArrived updatesArrived =
           config.OnError e
           do! Async.Sleep 1000
           return! loopAsync offset
-          
+
         | ex ->
           config.OnError ex
           // in case of "general" error we should increment offset to skip problematic update
@@ -149,7 +149,7 @@ let private runBot config me updateArrived updatesArrived =
 
       while listener.IsListening do
         let! context = listener.GetContextAsync() |> Async.AwaitTask
-        
+
         if validateRequest context.Request then
           match Funogram.Tools.parseJsonStream<Update> context.Request.InputStream with
           | Ok updates -> processUpdates [| updates |]
@@ -157,17 +157,42 @@ let private runBot config me updateArrived updatesArrived =
         else
           context.Response.StatusCode <- 403
         context.Response.Close()
-          
+
     }
-    
+
 let startBot config updateArrived updatesArrived =
   async {
     let! me = Api.getMe |> api config
-    return! me 
+    return! me
     |> function
     | Error error -> failwith error.Description
     | Ok me -> runBot config me updateArrived updatesArrived
   }
+
+let processUpdate config inputStream updateArrived updatesArrived =
+  let asyncMe = async {
+      let! me = Api.getMe |> api config
+      return me
+      |> function
+      | Ok e -> e
+      | Error error -> failwith error.Description
+    }
+  let me = asyncMe |> Async.RunSynchronously
+
+
+  let bot data = api config data
+  let processUpdates updates =
+    if updates |> Seq.isEmpty |> not then
+      updates |> Seq.iter (fun f -> updateArrived { Update = f; Config = config; Me = me })
+      updatesArrived |> Option.iter (fun x -> x updates)
+  let input =Funogram.Tools.parseJsonStream<Update> inputStream
+  match input with
+          | Ok updates -> processUpdates [| updates |]
+          | Error e -> config.OnError e
+  match input with
+          | Ok updates -> Some updates
+          | Error e -> None
+
 
 let processCommands (context: UpdateContext) =
   Seq.forall (fun command -> command context)
