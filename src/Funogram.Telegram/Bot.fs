cf9ad022e98ab90e168dc4c297f9132555cda098
marctdt <marc.tdt@oultook.com>
2023-10-02 11:08:06 +0800

    Broken Refactoring to handle async

--- src/Funogram.Telegram/Bot.fs
+++ src/Funogram.Telegram/Bot.fs
@@ -169,30 +169,5 @@ let startBot config updateArrived updatesArrived =
     | Ok me -> runBot config me updateArrived updatesArrived
   }
 
-let processUpdate config inputStream updateArrived updatesArrived =
-  let asyncMe = async {
-      let! me = Api.getMe |> api config
-      return me
-      |> function
-      | Ok e -> e
-      | Error error -> failwith error.Description
-    }
-  let me = asyncMe |> Async.RunSynchronously
-
-
-  //let bot data = api config data
-  let processUpdates updates =
-    if updates |> Seq.isEmpty |> not then
-      updates |> Seq.iter (fun f -> updateArrived { Update = f; Config = config; Me = me })
-      updatesArrived |> Option.iter (fun x -> x updates)
-  let input =Funogram.Tools.parseJsonStream<Update> inputStream
-  match input with
-          | Ok updates -> processUpdates [| updates |]
-          | Error e -> config.OnError e
-  match input with
-          | Ok updates -> Some updates
-          | Error e -> None
-
-
 let processCommands (context: UpdateContext) =
   Seq.forall (fun command -> command context)
---
+++ src/Funogram.Telegram/BotExtension.fs
@@ -0,0 +1,42 @@
+﻿module Funogram.Telegram.BotExtension
+
+open Funogram.Telegram.Bot
+open System
+open System.IO
+open System.Net.Http
+open Funogram.Telegram
+open Funogram.Telegram.Sscanf
+open Funogram.Telegram.Types
+open Funogram.Types
+open Funogram.Api
+
+let processUpdateAsync config inputStream updateArrivedAsync updatesArrivedAsync=
+    async {
+
+        let! meAsync =
+            Api.getMe |> api config
+                // me
+        let test = meAsync
+        |> function
+            | Ok e -> e
+            | Error error -> failwith error.Description
+
+        // let me = asyncMe |> Async.RunSynchronously
+
+      //let bot data = api config data
+        let processUpdates updates =
+          if updates |> Seq.isEmpty |> not then
+            updates |> Seq.iter (fun f -> updateArrived { Update = f; Config = config; Me = me })
+            updatesArrived |> Option.iter (fun x -> x updates)
+        let input =Funogram.Tools.parseJsonStream<Update> inputStream
+        match input with
+                | Ok updates -> processUpdates [| updates |]
+                | Error e -> config.OnError e
+        match input with
+                | Ok updates -> Some updates
+                | Error e -> None
+
+
+  }
+
+let processUpdate config inputStream updateArrived updatesArrived =
